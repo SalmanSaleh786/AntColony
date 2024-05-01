b40f22d69e9072b3d8552dba4014ea3f35c9d99b
Salman Saleh <salmansaleh786@gmail.com>
2024-05-01 15:41:40 +0500

    Final Updated Code

--- SBSE_Project_2/MainWindow.xaml.cs
+++ SBSE_Project_2/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ using Windows.UI.Popups;
 namespace SBSE_Project_2
 {
     /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// An empty window that. can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainWindow : Window
     {
